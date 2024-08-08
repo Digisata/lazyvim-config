c29497003f84b2a83ea275138782f7afd7f82afb
naufal <khairunnaufal@transnovasi.id>
2024-08-08 10:44:36 +0700

    adjust dap conf

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -7,12 +7,14 @@ return {
       "jay-babu/mason-nvim-dap.nvim",
       "theHamsta/nvim-dap-virtual-text",
       "nvim-neotest/nvim-nio",
+      "nvim-neo-tree/neo-tree.nvim",
     },
     config = function()
       require("mason-nvim-dap").setup({})
       require("nvim-dap-virtual-text").setup()
 
       local dap, dapui = require("dap"), require("dapui")
+
       vim.keymap.set("n", "<F2>", dap.step_into)
       vim.keymap.set("n", "<F3>", dap.step_over)
       vim.keymap.set("n", "<F4>", dap.step_out)
@@ -20,18 +22,19 @@ return {
       vim.keymap.set("n", "<F6>", dap.restart)
       vim.keymap.set("n", "<F7>", dap.terminate)
 
-      dap.listeners.before.attach.dapui_config = function()
+      local function open_dapui()
         dapui.open()
+        vim.cmd("Neotree close")
       end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
+
+      local function close_dapui()
         dapui.close()
+        vim.cmd("Neotree show")
       end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
+
+      dap.listeners.before.event_initialized["dapui_config"] = open_dapui
+      dap.listeners.before.event_terminated["dapui_config"] = close_dapui
+      dap.listeners.before.event_exited["dapui_config"] = close_dapui
 
       require("dapui").setup()
     end,
