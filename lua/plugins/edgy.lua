5c09e003236b5c4b6263627f59cf837de3eb3dab
naufal <khairunnaufal@transnovasi.id>
2024-11-10 09:23:16 +0700

    update

--- lua/plugins/edgy.lua
+++ lua/plugins/edgy.lua
@@ -39,12 +39,11 @@ return {
     left = {
       -- Neo-tree filesystem always takes half the screen height
       {
-        title = "Neo-Tree",
         ft = "neo-tree",
         filter = function(buf)
           return vim.b[buf].neo_tree_source == "filesystem"
         end,
-        size = { height = 0.5 },
+        size = { height = 0.5, width = 0.2 },
       },
     },
   },
